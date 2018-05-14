4b51e6599986fdf8d2fc16edf075678ec99e2d41
haqi <mhaqiyusuf@gmail.com>
2018-05-15 06:15:10 +0700

    update

--- app/src/main/java/com/example/berbagi/homeFragment.java
+++ app/src/main/java/com/example/berbagi/homeFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -22,8 +24,19 @@ public class homeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        String[] menuItems = {"Donasi 1", "Donasi Bencana","Donasi 2"};
+        ListView listView = (ListView) view.findViewById(R.id.listView);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                menuItems
+        );
+
+        listView.setAdapter(listViewAdapter);
+
+        return view;
     }
 
 }
