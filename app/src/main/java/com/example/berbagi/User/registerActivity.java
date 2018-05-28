5505d2fce9dc4a8497cee1b5ac7961e9a164f6bd
haqi <mhaqiyusuf@gmail.com>
2018-05-28 23:36:45 +0700

    Update to 1.02 Beta

---
+++ app/src/main/java/com/example/berbagi/User/registerActivity.java
@@ -0,0 +1,15 @@
+package com.example.berbagi.User;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.berbagi.R;
+
+public class registerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
